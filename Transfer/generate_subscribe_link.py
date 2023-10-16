82791e5c4b41f2108ddb057ed20f2bbee587b2d1
YvanW <128964123+Smurf-Yvan@users.noreply.github.com>
2023-10-16 03:13:55 +0000

    test

---
+++ Transfer/generate_subscribe_link.py
@@ -0,0 +1,21 @@
+import json
+import base64
+
+# 读取配置文件
+with open("config.json", "r") as config_file:
+    server_config = json.load(config_file)
+
+# 将配置信息转换为JSON字符串
+config_json = json.dumps(server_config)
+
+# 将JSON字符串进行Base64编码
+base64_encoded_config = base64.urlsafe_b64encode(config_json.encode()).decode()
+
+# 构建订阅链接
+subscribe_link = f"vmess://{base64_encoded_config}"
+
+# 将订阅链接写入文件
+with open("V2ray.txt", "w") as output_file:
+    output_file.write(subscribe_link)
+
+print("订阅链接已保存到 V2ray.txt 文件中。")
